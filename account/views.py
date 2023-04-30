689db637f0799faf43ed2ebd4477221aa9d638b1
ademiniiazbekkyzy <ademi.niiazbekkyzy@mail.ru>
2023-04-30 23:34:22 +0600

    account

--- account/views.py
+++ account/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 
+from account.models import MenuItem, OrderModel
+
 
 class Index(View):
     def get(self, request, *args, **kwargs):
@@ -10,4 +12,61 @@ class Index(View):
 class About(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'account/about.html')
+
+
+class Order(View):
+    def get(self, request, *args, **kwargs):
+
+        # get every item from each category
+        appetizers = MenuItem.objects.filter(category__name__contains="Appetizer")
+        entres = MenuItem.objects.filter(category__name__contains="Entre")
+        desserts = MenuItem.objects.filter(category__name__contains="Dessert")
+        drinks = MenuItem.objects.filter(category__name__contains="Drink")
+
+        # pass into context
+        context = {
+            'appetizers': appetizers,
+            'drinks': drinks,
+            'entres': entres,
+            'desserts': desserts,
+        }
+
+        # render the template
+        return render(request, 'account/order.html', context)
+
+    def post(self, request, *args, **kwargs):
+        order_items = {
+            'items': []
+        }
+
+        items = request.POST.getlist('items[]')
+
+        for item in items:
+            menu_item = MenuItem.objects.get(pk__contains=int(item))
+            item_data = {
+                'id': menu_item.pk,
+                'name': menu_item.name,
+                'price': menu_item.price,
+            }
+
+            order_items['items'].append(item_data)
+
+            price = 0
+            item_ids = []
+
+            for item in order_items['items']:
+                price += item['price']
+                item.ids.append(item['id'])
+
+            order = OrderModel.objects.create(price=price)
+            order.items.add(*item_id)
+
+            context = {
+                'items': order_items['items'],
+                'price': price,
+            }
+
+            return render(request, 'account/order_confirmation.html')
+
+
 
